2cba675866deb6ed6082b63cece27a63de590b01
mani <111manikanta.v@gmail.com>
2023-07-05 17:12:47 +0530

    added challenge fixBrokenStory.js

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,5 +1,15 @@
+import { StoryTray } from './challanges/fixBrokenStoryTRay'
+
+
+
+
 export default function App() {
 
-  return (<h1>hi started again</h1>
+  return (
+
+    <>
+      <h1>hi started again</h1>
+      <StoryTray></StoryTray>
+    </>
   )
 }
